4bf5f286aad2abd3e6597d4f55194a21481a7d99
RyanBerriault <rynoberriault@gmail.com>
2017-12-05 05:51:21 -0500

    First Start
    Menu UI System works, Now to add all of the examples one by one.

---
+++ SampleProgram/Assets/Scripts/ChangeSceneTo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneTo : MonoBehaviour 
+{
+	public void SwitchTo(int index)
+	{
+		SceneManager.LoadScene(index);
+	}
+}
---
+++ SampleProgram/Assets/Scripts/ExitToDesktop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitToDesktop : MonoBehaviour
+{
+	public void ExitProgram()
+	{
+		 #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+         Application.Quit();
+        #endif
+		Debug.Log("Program Terminated");
+	}
+}
